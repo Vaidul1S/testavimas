2e061dd660a4f83ada72c895181ee63a462736ed
Vaidul1s <vaidas.ad25@gmail.com>
2025-01-31 10:10:17 +0200

    01.31

--- CRUD_API/server.js
+++ CRUD_API/server.js
@@ -7,6 +7,14 @@ app.use(express.json());
 
 //-----------------------------Routes-----------------------------
 
+app.get('/products', async (req, res) => {
+    try {
+        res.status(200).json({message: 'Sėkmingai prisijungta'});
+    } catch (error) {
+        res.status(400).json({error: 'error'});
+    }
+    
+});
 
 app.listen(port, () => {
     console.log(`CRUD API darbui pasiruošus ant ${port} porto!`)
