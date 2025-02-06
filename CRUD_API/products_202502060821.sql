2d368c0a6b084f28553c7e5503711fbcafef7109
Vaidul1s <vaidas.ad25@gmail.com>
2025-02-06 08:22:23 +0200

    02.06

---
+++ CRUD_API/products_202502060821.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.products ("name",price,description) VALUES
+	 ('tinklinio_kamuolys',80.0,'molten v5m5000'),
+	 ('krepsinio_kamuolys',65.0,'molten bg4000'),
+	 ('futbolo_kamuolys',55.0,'select v23'),
+	 ('teniso_kamuoliukas',9.0,'dunlop'),
+	 ('ledo_ritulys',8.0,'nijdam'),
+	 ('naujasproduktas',12345678,'labai brangus produktas');
