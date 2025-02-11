f388e53076a12cbd0c216ef2ea8124c54f009f87
Vaidul1s <vaidas.ad25@gmail.com>
2025-02-11 08:13:45 +0200

    02.11

---
+++ CRUD_API/cypress/e2e/API-test/api-test.cy.js
@@ -0,0 +1,58 @@
+// const { response } = require("express");   --------------- uzkomentuoti, express knisa prota cypressui!!!!!!!!!!!!!!
+
+describe('CRUD_API', () => {
+
+    context('/products', () => {
+
+        it('GET all productts', () => {
+            cy.request("GET", "localhost:5555/products").then((res) => {
+                expect(res.status).to.eq(200);
+                cy.log(res.body);
+                expect(res.body).length.to.greaterThan(0);
+                expect(res.duration).to.lessThan(2000);
+                expect(res.duration).to.below(500);
+            });
+        });
+
+        it('Status check', () => {
+            cy.request("GET", "localhost:5555/products").then((res) => {
+                expect(res.status).to.eq(200);
+            });
+        });
+
+        it('GET a single product', () => {
+            cy.request("GET", "localhost:5555/products/5").then((res) => {
+                expect(res.status).to.eq(200);                
+                cy.log(res.body);
+                cy.log(res.body.id);
+                cy.log(res.body.name);
+                cy.log(res.body.price);
+                cy.log(res.body.description);
+                expect(res.body).to.have.property('id', 5);
+            });
+        });
+
+        it('CREATE a product', () => {
+            cy.request("POST", "localhost:5555/products", {name: 'cyPreke', price: 0.99, description: 'pigi test preke'}).then((res) => {
+
+                expect(res.status).to.eq(201);
+                cy.log(res.body);
+            });
+        });
+
+        it('UPDATE a product', () => {
+            cy.request("PUT", "localhost:5555/products/6", {name: 'cyPreke', price: 1.99, description: 'pigi test preke'}).then((res) => {
+                expect(res.status).to.eq(200);
+                cy.log(res.body);
+            });
+        });
+
+        it('DELETE a product', () => {
+            cy.request("DELETE", "localhost:5555/products/7").then((res) => {
+                expect(res.status).to.eq(200);
+                cy.log(res.body);
+            });
+        });
+
+    })
+});
