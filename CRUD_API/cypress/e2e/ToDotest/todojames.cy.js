d8e5ce52464e924eeb480a76a9eff623bc89fbbd
Vaidul1s <vaidas.ad25@gmail.com>
2025-02-12 10:04:46 +0200

    02.12

--- CRUD_API/cypress/e2e/ToDotest/todojames.cy.js
+++ CRUD_API/cypress/e2e/ToDotest/todojames.cy.js
@@ -1,4 +1,4 @@
-describe('ToDoTest', () => { 
+describe('ToDoTest', () => {
 
     it('visit ToDo site', () => {
         cy.visit('https://todolist.james.am/#/');
@@ -6,7 +6,7 @@ describe('ToDoTest', () => {
 
         cy.get('ul.todo-list li').contains('do something cool').should('be.visible').and('have.text', 'do something cool');
         cy.contains('ul.todo-list li', 'do something cool').find('button.destroy').invoke('show');
-        
+
     });
 
     it('Delete ToDo taks', () => {
@@ -17,7 +17,19 @@ describe('ToDoTest', () => {
         cy.contains('ul.todo-list li', 'trinama uzduotis').find('button.destroy').invoke('show');
         cy.contains('ul.todo-list li', 'trinama uzduotis').find('button.destroy').click();
 
-        
+    });
 
-    })
+    it('Edit ToDo taks', () => {
+        cy.visit('https://todolist.james.am/#/');
+        cy.get('.new-todo').type('task for editing{enter}');
+        cy.get('.new-todo').type('2 uzduotis{enter}');
+
+        cy.contains('ul.todo-list li', 'task for editing').dblclick().type('{selectAll}{del}edited task{enter}');
+        cy.contains('ul.todo-list li', '2 uzduotis')
+            .dblclick()
+            .find('input.edit')
+            .clear()
+            .type('redaguota uzduotis{enter}');
+    });
+
 });
