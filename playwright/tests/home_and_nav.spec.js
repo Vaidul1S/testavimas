1b18f805a5aa5baa3c1aaf07556ac67c792f2b67
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-10 08:57:00 +0200

    03.10

--- playwright/tests/home_and_nav.spec.js
+++ playwright/tests/home_and_nav.spec.js
@@ -2,14 +2,13 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Home Page and Navigation', () => {
 
-    test('1. Home Page', async ({ page }) => {
+    test.beforeEach(async ({ page }) => {
         await page.goto('https://sweetshop.netlify.app/');
-        await expect(page.locator('.display-3')).toHaveText('Welcome to the sweet shop!');
-        await expect(page.locator('nav.navbar')).toBeVisible(); 
     });
 
-    test.beforeEach(async ({page}) => {
-        await page.goto('https://sweetshop.netlify.app/');
+    test('1. Home Page', async ({ page }) => {
+        await expect(page.locator('.display-3')).toHaveText('Welcome to the sweet shop!');
+        await expect(page.locator('nav.navbar')).toBeVisible();
     });
 
     test('2. Navigation route - Sweets', async ({ page }) => {
@@ -18,7 +17,23 @@ test.describe('Home Page and Navigation', () => {
     });
 
     test('3. Navigation route - About', async ({ page }) => {
-        await page.getByRole('link', { name: 'About', exact: true }).click();
+        await page.getByRole('link', { name: 'About' }).click();
         await expect(page).toHaveURL('https://sweetshop.netlify.app/about');
     });
+
+    test('4. Navigation route - Login', async ({ page }) => {
+        await page.getByRole('link', { name: 'Login' }).click();
+        await expect(page).toHaveURL('https://sweetshop.netlify.app/login');
+    });
+
+    test('5. Navigation route - Basket', async ({ page }) => {
+        await page.getByRole('link', { name: 'Basket' }).click();
+        await expect(page).toHaveURL('https://sweetshop.netlify.app/basket');
+    });
+
+    test('6. Navigation route - Sweet Shop', async ({ page }) => {
+        await page.getByRole('link', { name: 'Sweet Shop' }).click();
+        await expect(page).toHaveURL('https://sweetshop.netlify.app');
+    });
+
 });
