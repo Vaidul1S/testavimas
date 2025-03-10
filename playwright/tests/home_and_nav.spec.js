40fc8ce7f41f61b966d80ae0c52b36498f5c05c9
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-10 08:35:28 +0200

    03.10

--- playwright/tests/home_and_nav.spec.js
+++ playwright/tests/home_and_nav.spec.js
@@ -1,13 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://sweetshop.netlify.app/');
-  await expect(page.locator('h1')).toContainText('Welcome to the sweet shop!');
-  await expect(page.locator('#navbarColor01')).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Welcome to the sweet shop!' })).toBeVisible();
-  await page.getByRole('link', { name: 'Sweets', exact: true }).click();
-  await page.getByRole('link', { name: 'About' }).click();
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByRole('link', { name: 'Basket' }).click();
-  await page.getByRole('link', { name: 'Sweet Shop' }).click();
+test.describe('Home Page and Navigation', () => {
+
+    test('Home Page', async ({ page }) => {
+        await page.goto('https://sweetshop.netlify.app/');
+        await expect(page.locator('h1')).toContainText('Welcome to the sweet shop!').toBeVisible();
+        await expect(page.locator('#nav.navbar')).toBeVisible();
+        await page.getByRole('link', { name: 'Sweets', exact: true }).click();
+        await page.getByRole('link', { name: 'About' }).click();
+        await page.getByRole('link', { name: 'Login' }).click();
+        await page.getByRole('link', { name: 'Basket' }).click();
+        await page.getByRole('link', { name: 'Sweet Shop' }).click();
+    });
+
 });
