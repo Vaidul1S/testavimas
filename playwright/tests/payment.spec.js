3ea7577502b3d907c4b2b09a5199bce04ecb3ec2
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-12 08:03:59 +0200

    03.12

---
+++ playwright/tests/payment.spec.js
@@ -0,0 +1,54 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Checkout and Payment', () => {
+    
+    test('1. Filling checkout form successfully', async ({ page }) => {
+        await page.goto('https://sweetshop.netlify.app/');
+        await page.getByRole('link', { name: 'Basket' }).click();
+        await expect(page).toHaveURL('https://sweetshop.netlify.app/basket');
+
+        await page.locator('#name').nth(0).fill('Bebras');
+        await page.locator('#name').nth(1).fill('Babrauskas');
+        await page.fill('#email', 'bebras@example.com');
+        await page.fill('#address', 'Rastu 11');
+        await page.fill('#address2', 'Bebriskes');
+        await page.selectOption('#country', 'United Kingdom');
+        await page.selectOption('#city', 'Bristol');
+        await page.fill('#zip', '123456');
+        await page.fill('#cc-name', '123456');
+        await page.fill('#cc-number', '123456');
+        await page.fill('#cc-expiration', '123456');
+        await page.fill('#cc-cvv', '123456');
+        
+        await page.getByRole('button', { name: 'Continue to checkout' }).click();
+    });
+
+    test('2. Filling checkout form unsuccessfully', async ({ page }) => {
+        await page.goto('https://sweetshop.netlify.app/');
+        await page.getByRole('link', { name: 'Basket' }).click();
+        await expect(page).toHaveURL('https://sweetshop.netlify.app/basket');
+
+        await page.getByRole('button', { name: 'Continue to checkout' }).click();
+
+        // await expect(page.locator('.invalid-feedback').nth(1)).toContainText('Valid first name is required.');
+
+        const errorMessages = [
+            'Valid first name is required.',
+            'Valid last name is required.',
+            'Please enter a valid email address for shipping updates.',
+            'Please enter your shipping address.',
+            'Please select a valid country.',
+            'Please provide a valid state.',
+            'Zip code required.',
+            'Name on card is required',
+            'Credit card number is required',
+            'Expiration date required',
+            'Security code required'
+        ];
+
+        for (let i = 0; i <= 10; i++) {
+            await expect(page.locator('.invalid-feedback').nth(i + 1)).toContainText(errorMessages[i]);
+        }
+    });
+
+});
---
+++ playwright/tests/products.spec.js
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Products', () => {
+    
+    test('1. Product Page', async ({ page }) => {
+        await page.goto('https://sweetshop.netlify.app/');
+        await expect(page.getByRole('link', { name: 'Browse Sweets' })).toBeVisible();
+        await page.getByRole('link', { name: 'Browse Sweets' }).click();
+    });
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://sweetshop.netlify.app/sweets');
+    });
+
+    test('2. Products have images', async ({ page }) => {
+        for (let i = 0; i < page.locator('.card-img-top').length; i++) {
+            await expect(page.locator('.card-img-top').nth(i)).toBeVisible();            
+        }
+                
+    });
+
+    test('3. Products have names', async ({ page }) => {
+        for (let i = 0; i < page.locator('.card-title').length; i++) {
+            await expect(page.locator('.card-title').nth(i)).toBeVisible();            
+        }
+    });
+
+    test('4. Products have text', async ({ page }) => {
+        for (let i = 0; i < page.locator('.card-text').length; i++) {
+            await expect(page.locator('.card-text').nth(i)).toBeVisible();            
+        }
+    });
+
+    test('5. Products have prices', async ({ page }) => {
+        for (let i = 0; i < page.locator('.text_muted').length; i++) {
+            await expect(page.locator('.text_muted').nth(i)).toBeVisible();            
+        }
+    });
+
+    test('6. Products have add to basket button', async ({ page }) => {
+        for (let i = 0; i < page.locator('.add-item').length; i++) {
+            await expect(page.locator('.add-item').nth(i)).toBeVisible();            
+        }
+    });
+
+});
