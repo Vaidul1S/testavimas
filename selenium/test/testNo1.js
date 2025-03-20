323eb0305639ab58a7c7cf6cebc0a2c3eb5daf08
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-20 09:55:06 +0200

    03.20

--- selenium/test/testNo1.js
+++ selenium/test/testNo1.js
@@ -1,19 +1,25 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-// import { expect } from 'chai';
-// const assert = require('assert'); -- neveikia, cia to grybo paistalai
+const { Builder, By, Key, until } = require('selenium-webdriver');
+// import { expect } from 'chai';   -- neveikia
+// const assert = require('assert'); -- neveikia
 
+const { expect, should } = import('chai');
 
-describe('Todo testing', function() {
-    it('Add new to do', async function() {
+describe('Todo testing', function () {
+    it('Add new to do', async function () {
         driver = await new Builder().forBrowser('chrome').build();
         await driver.get('https://todolist.james.am/#/');
-        await driver.wait(until.elementLocated(By.className('new-todo')),2000);
+        await driver.wait(until.elementLocated(By.className('new-todo')), 2000);
 
         const newTodoInput = await driver.findElement(By.className('new-todo'));
 
         newTodoInput.sendKeys('Pirma uzduotis', Key.RETURN);
-        
-        const addedTodoItem = await driver.findElement(By.xpath("//li[contains(text()='Pirma uzduotis')]"))
+        await driver.sleep(2000);
+
+        const addedTodoItem = await driver.findElement(By.xpath("//label[text()='Pirma uzduotis']"));
+
+        addedTodoItem.should.equal('Pirma uzduotis');
+        // expect(addedTodoItem).to.include('Pirma uzduotis');
+        console.log(addedTodoItem);
 
         await driver.sleep(2000);
 
--- selenium/test/testNo2.js
+++ selenium/test/testNo2.js
@@ -1,2 +1,28 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-// const should = require('chai').should();
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const should = require('chai');
+
+describe('editing todo', function () {
+    after(async function () {
+        await driver.quit();
+    });
+
+    it('should edit todo', async function () {
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.get('https://todolist.james.am/#/');
+        await driver.wait(until.elementLocated(By.className('new-todo')), 2000);
+
+        const newTodoInput = await driver.findElement(By.className('new-todo'));
+
+        await newTodoInput.sendKeys('1 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('2 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('3 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('4 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('5 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('6 uzduotis', Key.RETURN);
+        await driver.sleep(1000);
+
+        const editableTodo = await driver.findElement(By.xpath("//ul[@class='todo-list']/li[contains(., '3 uzduotis')]"));
+
+    })
+
+})
