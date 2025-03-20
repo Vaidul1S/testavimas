50e2330462b81191ada776bb3316593d08496f16
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-20 08:15:18 +0200

    03.20

--- selenium/test/testNo1.js
+++ selenium/test/testNo1.js
@@ -1,2 +1,8 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const should = require('chai').should();
+
+describe('Todo testing', function() {
+    it('Add new to do', async function() {
+        driver = await new Builder().forBrowser('chrome').build();
+    })
+})
