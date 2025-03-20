3aad7797c9271ba316774eefd3b42ddfef57e4ab
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-20 08:56:47 +0200

    03.20

--- selenium/test/testNo1.js
+++ selenium/test/testNo1.js
@@ -1,9 +1,19 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
-// const should = require('chai').should();
+// import { expect } from 'chai';
+// const assert = require('assert'); -- neveikia, cia to grybo paistalai
+
 
 describe('Todo testing', function() {
     it('Add new to do', async function() {
         driver = await new Builder().forBrowser('chrome').build();
+        await driver.get('https://todolist.james.am/#/');
+        await driver.wait(until.elementLocated(By.className('new-todo')),2000);
+
+        const newTodoInput = await driver.findElement(By.className('new-todo'));
+
+        newTodoInput.sendKeys('Pirma uzduotis', Key.RETURN);
+        
+        
         await driver.sleep(2000);
 
         await driver.quit();
