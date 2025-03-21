4d39f6c13c12b7825f660664511a41cd7c829173
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-21 09:11:17 +0200

    03.21

--- selenium/test/testNo1.js
+++ selenium/test/testNo1.js
@@ -1,28 +1,26 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
-// import { expect } from 'chai';   -- neveikia
-// const assert = require('assert'); -- neveikia
 
-const { expect, should } = import('chai');
+(async () => {
+    const should = await import('chai').should();
+        
+    describe('Todo testing', function () {
+        
+        it('Add new to do', async function () {
+            driver = await new Builder().forBrowser('chrome').build();
+            await driver.get('https://todolist.james.am/#/');
+            await driver.wait(until.elementLocated(By.className('new-todo')), 2000);
 
-describe('Todo testing', function () {
-    it('Add new to do', async function () {
-        driver = await new Builder().forBrowser('chrome').build();
-        await driver.get('https://todolist.james.am/#/');
-        await driver.wait(until.elementLocated(By.className('new-todo')), 2000);
+            const newTodoInput = await driver.findElement(By.className('new-todo'));
 
-        const newTodoInput = await driver.findElement(By.className('new-todo'));
+            newTodoInput.sendKeys('Pirma uzduotis', Key.RETURN);
+            await driver.sleep(2000);
 
-        newTodoInput.sendKeys('Pirma uzduotis', Key.RETURN);
-        await driver.sleep(2000);
+            const addedTodoItem = await driver.findElement(By.xpath("//label[text()='Pirma uzduotis']")).getText();
 
-        const addedTodoItem = await driver.findElement(By.xpath("//label[text()='Pirma uzduotis']"));
+            addedTodoItem.should.equal('Pirma uzduotis');
+            console.log('Added todo:', addedTodoItem);
 
-        addedTodoItem.should.equal('Pirma uzduotis');
-        // expect(addedTodoItem).to.include('Pirma uzduotis');
-        console.log(addedTodoItem);
-
-        await driver.sleep(2000);
-
-        await driver.quit();
+            await driver.quit();
+        });
     });
-});
+})();
--- selenium/test/testNo2.js
+++ selenium/test/testNo2.js
@@ -1,5 +1,6 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
-const should = require('chai');
+// const should = require('chai');
+const assert = require('assert');
 
 describe('editing todo', function () {
     after(async function () {
@@ -19,10 +20,24 @@ describe('editing todo', function () {
         await newTodoInput.sendKeys('4 uzduotis', Key.RETURN);
         await newTodoInput.sendKeys('5 uzduotis', Key.RETURN);
         await newTodoInput.sendKeys('6 uzduotis', Key.RETURN);
-        await driver.sleep(1000);
+        await driver.sleep(2000);
 
         const editableTodo = await driver.findElement(By.xpath("//ul[@class='todo-list']/li[contains(., '3 uzduotis')]"));
 
-    })
+        driver.actions().doubleClick(editableTodo).perform();
 
-})
+        const editField = await editableTodo.findElement(By.css('input.edit'));
+
+        for(let i=0; i<100;i++) {
+            await editField.sendKeys(Key.BACK_SPACE);
+        };
+
+        await editField.sendKeys('edited todo', Key.RETURN);
+
+        const editedItem = await driver.findElement(By.xpath("//ul[@class='todo-list']//li[contains(., 'edited todo')]")).getText();
+        await driver.sleep(3000);
+
+        assert.strictEqual(editedItem, 'edited todo', 'Edit failed');
+    });
+
+});
