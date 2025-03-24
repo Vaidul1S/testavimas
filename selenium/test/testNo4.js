0602f9c97ffb5840c99dad15670d7589984bcaa9
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-24 09:48:14 +0200

    03.24

--- selenium/test/testNo4.js
+++ selenium/test/testNo4.js
@@ -1,4 +1,5 @@
 import { Builder, By, Key, until } from 'selenium-webdriver';
+import { expect } from 'chai';
 
 (async () => {
     describe('Todo testing', function () {
@@ -8,7 +9,22 @@ import { Builder, By, Key, until } from 'selenium-webdriver';
             try {
                 await driver.get('https://todolist.james.am/#/');
 
-                
+                await driver.wait(until.elementLocated(By.className('new-todo')), 2000);
+
+                const newTodoInput = await driver.findElement(By.className('new-todo'));
+                await newTodoInput.sendKeys('1 uzduotis', Key.RETURN);
+                await newTodoInput.sendKeys('2 uzduotis', Key.RETURN);
+                await newTodoInput.sendKeys('3 uzduotis', Key.RETURN);
+                await newTodoInput.sendKeys('4 uzduotis', Key.RETURN);
+
+                const activeBtn = await driver.findElement(By.xpath("//ul/li[2]/a[contains(., 'active')]"));
+
+                await activeBtn.click();
+
+                const activeList = await driver.findElement(By.className('todo-list'));
+
+                expect(activeList).length.to.equal('4');
+
             } finally {
                 await driver.quit();
             }
