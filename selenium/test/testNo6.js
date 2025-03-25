ce70899c42c32fc1ea31cdd5941b1b0a0bf50590
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-25 23:19:16 +0200

    03.25

--- selenium/test/testNo6.js
+++ selenium/test/testNo6.js
@@ -12,6 +12,16 @@ describe('Todo testing', function () {
         await driver.quit();
     });
 
-    
+    it('Count completed tasks', async function () {
+        await driver.get('https://todolist.james.am/#/');
 
+        await driver.wait(until.elementLocated(By.className('new-todo')), 3000);
+
+        const newTodoInput = await driver.findElement(By.className('new-todo'));
+        await newTodoInput.sendKeys('1 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('2 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('3 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('4 uzduotis', Key.RETURN);
+
+    });
 });
