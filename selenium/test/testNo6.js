3669d3dda75af00603cd4a861f6e801bec5027d2
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-25 23:18:12 +0200

    03.25

--- selenium/test/testNo6.js
+++ selenium/test/testNo6.js
@@ -1,3 +1,17 @@
 import { Builder, By, Key, until } from 'selenium-webdriver';
 import { expect } from 'chai';
 
+describe('Todo testing', function () {
+    let driver;
+
+    before(async function () {
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+
+    after(async function () {
+        await driver.quit();
+    });
+
+    
+
+});
