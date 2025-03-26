e78a3e209f84796dc7f869516c23adbbd7a3e63a
Vaidul1s <vaidas.ad25@gmail.com>
2025-03-26 12:04:51 +0200

    03.26

---
+++ selenium/test/testNo7.js
@@ -0,0 +1,31 @@
+import { Builder, By, Key, until } from 'selenium-webdriver';
+import { expect } from 'chai';
+
+describe('Todo testing', function () {
+    let driver;
+
+    before(async function () {
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+
+    after(async function () {
+        await driver.quit();
+    });
+
+    it('Select all tasks at once', async function () {
+        await driver.get('https://todolist.james.am/#/');
+
+        await driver.wait(until.elementLocated(By.className('new-todo')), 3000);
+
+        const newTodoInput = await driver.findElement(By.className('new-todo'));
+        await newTodoInput.sendKeys('1 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('2 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('3 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('4 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('5 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('6 uzduotis', Key.RETURN);
+
+        
+
+    });
+});
