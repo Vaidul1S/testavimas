e5857a17e5ab65d9b7be86eb9723b1deafa213e3
Vaidul1S <vaidas.ad25@gmail.com>
2025-04-30 10:53:01 +0300

    04.30

--- selenium/test/autoN01.js
+++ selenium/test/autoN01.js
@@ -12,6 +12,15 @@ import { expect } from 'chai';
                     until.urlIs('https://automationexercise.com/'),
                     5000
                 );
+                await driver.wait(until.elementLocated(By.xpath('/html/body/div/div[2]/div[2]/div[2]/div[2]/button[1]')), 5000).click(); //accept cookies
+                await driver.findElement(By.linkText('Signup / Login')).click();
+                await driver.wait(until.urlIs('https://automationexercise.com/login'), 10000);
+                await driver.wait(until.elementLocated(By.css('.signup-form')), 5000);
+
+                await driver.findElement(By.css('input[data-qa="signup-name"]')).sendKeys('Bebras666');
+                await driver.findElement(By.css('input[data-qa="signup-email"]')).sendKeys('bebras666@example.com');
+                await driver.findElement(By.css('button[data-qa="signup-button"]')).click();
+
                 console.log("✅ Test completed successfully.");
 
             } catch (error) {
