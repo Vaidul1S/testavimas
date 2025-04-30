88a180109f522bab672889439c0921a07016d4e3
Vaidul1S <vaidas.ad25@gmail.com>
2025-04-30 10:31:50 +0300

    04.30

---
+++ selenium/test/autoN01.js
@@ -0,0 +1,31 @@
+import { Builder, By, Key, until } from 'selenium-webdriver';
+import { expect } from 'chai';
+
+(async () => {
+    describe('Automation exercise testing', function () {
+        let driver;
+        it('01 Register new user', async function () {
+            driver = await new Builder().forBrowser('chrome').build();
+            try {
+                await driver.get('https://automationexercise.com/');
+
+                const newTodoInput = await driver.wait(
+                    until.elementLocated(By.className('new-todo')),
+                    5000
+                );
+                await newTodoInput.sendKeys('Pirma uzduotis', Key.RETURN);
+                const addedTodoItem = await driver.wait(
+                    until.elementLocated(By.xpath("//label[text()='Pirma uzduotis']")),
+                    5000
+                );
+
+                const addedTodoText = await addedTodoItem.getText();
+                expect(addedTodoText).to.equal('Pirma uzduotis');
+                console.log('Added to-do:', addedTodoText);
+            } finally {
+                // Quit the browser even if the test fails
+                await driver.quit();
+            }
+        });
+    });
+})();
