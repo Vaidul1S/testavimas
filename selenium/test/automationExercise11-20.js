d2f7b52bdcbd9a91a6a823f6d3d42a5fe0421d08
Vaidul1S <vaidas.ad25@gmail.com>
2025-05-05 11:00:24 +0300

    05.05

--- selenium/test/automationExercise11-20.js
+++ selenium/test/automationExercise11-20.js
@@ -79,7 +79,7 @@ import { expect } from 'chai';
             }
         });
 
-        it.only('13 Verify Product quantity in Cart', async function () {
+        it('13 Verify Product quantity in Cart', async function () {
             driver = await new Builder().forBrowser('chrome').build();
             try {
                 await driver.get('https://automationexercise.com/');
@@ -109,6 +109,115 @@ import { expect } from 'chai';
                 await driver.quit();
             }
         });
+
+        it.only('14 Place Order: Register while Checkout', async function () {
+            driver = await new Builder().forBrowser('chrome').build();
+            try {
+                await driver.get('https://automationexercise.com/');
+                await driver.wait(until.urlIs('https://automationexercise.com/'), 5000);
+                await driver.wait(until.elementLocated(By.xpath('/html/body/div/div[2]/div[2]/div[2]/div[2]/button[1]')), 5000).click(); //accept cookies
+                
+                const productInfo = await driver.findElements(By.css('div.productinfo'));
+                await driver.actions({ bridge: true }).move({ origin: productInfo[0] }).perform()
+                await driver.sleep(1000);
+                const addToCartBtn1 = await driver.wait(until.elementLocated(By.css('[data-product-id="1"]')), 5000);
+                
+                await driver.executeScript("arguments[0].scrollIntoView({block: 'center'});", addToCartBtn1);
+                await driver.wait(until.elementIsVisible(addToCartBtn1), 5000);
+                await driver.wait(until.elementIsEnabled(addToCartBtn1), 5000);
+                await addToCartBtn1.click();
+                await driver.sleep(1000);
+                await driver.wait(until.elementLocated(By.css('[data-dismiss="modal"]')), 5000).click();
+
+                const addToCartBtn2 = await driver.wait(until.elementLocated(By.css('[data-product-id="2"]')), 5000);
+                await driver.executeScript("arguments[0].scrollIntoView({block: 'center'});", addToCartBtn1);
+                await driver.wait(until.elementIsVisible(addToCartBtn2), 5000);
+                await driver.wait(until.elementIsEnabled(addToCartBtn2), 5000);
+                await addToCartBtn2.click();
+                await driver.sleep(1000);
+                await driver.wait(until.elementLocated(By.css('[data-dismiss="modal"]')), 5000).click();
+
+                await driver.actions({ bridge: true }).move({ origin: productInfo[2] }).perform()
+                await driver.sleep(1000);
+                const addToCartBtn3 = await driver.wait(until.elementLocated(By.css('[data-product-id="3"]')), 5000);                
+                await driver.executeScript("arguments[0].scrollIntoView({block: 'center'});", addToCartBtn3);
+                await driver.wait(until.elementIsVisible(addToCartBtn3), 5000);
+                await driver.wait(until.elementIsEnabled(addToCartBtn3), 5000);
+                await addToCartBtn3.click();
+                await driver.sleep(1000);
+                await driver.wait(until.elementLocated(By.css('[data-dismiss="modal"]')), 5000).click();
+
+                await driver.findElement(By.xpath('//*[@id="header"]/div/div/div/div[2]/div/ul/li[3]/a')).click();
+                await driver.wait(until.urlIs('https://automationexercise.com/view_cart'), 5000);
+
+                await driver.findElement(By.xpath('//*[@id="do_action"]/div[1]/div/div/a')).click();
+                await driver.sleep(1000);
+                await driver.findElement(By.xpath('//*[@id="checkoutModal"]/div/div/div[2]/p[2]/a')).click();
+
+                await driver.wait(until.urlIs('https://automationexercise.com/login'), 10000);
+                await driver.wait(until.elementLocated(By.css('.signup-form')), 5000);
+
+                await driver.findElement(By.css('input[data-qa="signup-name"]')).sendKeys('Bebras666');
+                await driver.findElement(By.css('input[data-qa="signup-email"]')).sendKeys('bebras666@example.com');
+                await driver.findElement(By.css('button[data-qa="signup-button"]')).click();
+
+                await driver.wait(until.elementLocated(By.xpath("//*[contains(text(),'Enter Account Information')]")), 5000);
+
+                await driver.findElement(By.id('id_gender1')).click();
+                await driver.findElement(By.css('input[data-qa="password"]')).sendKeys('password123');
+                await driver.findElement(By.id('days')).sendKeys('13');
+                await driver.findElement(By.id('months')).sendKeys('February');
+                await driver.findElement(By.id('years')).sendKeys('1999');
+                await driver.findElement(By.id('newsletter')).click();
+                await driver.findElement(By.id('optin')).click();
+                await driver.findElement(By.css('input[data-qa="first_name"]')).sendKeys('Bredas');
+                await driver.findElement(By.css('input[data-qa="last_name"]')).sendKeys('Babrauskas');
+                await driver.findElement(By.css('input[data-qa="company"]')).sendKeys('Uztvanka');
+                await driver.findElement(By.css('input[data-qa="address"]')).sendKeys('Bebriskes');
+                await driver.findElement(By.css('input[data-qa="address2"]')).sendKeys('Rastu 11');
+                await driver.findElement(By.css('select[data-qa="country"]')).sendKeys('Canada');
+                await driver.findElement(By.css('input[data-qa="state"]')).sendKeys('Big State');
+                await driver.findElement(By.css('input[data-qa="city"]')).sendKeys('Small City');
+                await driver.findElement(By.css('input[data-qa="zipcode"]')).sendKeys('1234567890');
+                await driver.findElement(By.css('input[data-qa="mobile_number"]')).sendKeys('1234567890');
+
+                await driver.findElement(By.css('button[data-qa="create-account"]')).click();
+
+                await driver.wait(until.elementLocated(By.xpath("//*[contains(text(),'Account Created!')]")), 5000);
+                await driver.findElement(By.css('[data-qa="continue-button"]')).click();
+                await driver.wait(until.elementLocated(By.linkText('Logged in as Bebras666')), 5000);
+
+                await driver.findElement(By.xpath('//*[@id="header"]/div/div/div/div[2]/div/ul/li[3]/a')).click();
+                await driver.wait(until.urlIs('https://automationexercise.com/view_cart'), 5000);
+
+                await driver.findElement(By.xpath('//*[@id="do_action"]/div[1]/div/div/a')).click();
+
+                await driver.findElement(By.css('textarea.form-control')).sendKeys('Description coment in comment text area.');
+                await driver.findElement(By.css('a[href="/payment"]')).click();
+                await driver.sleep(1000);
+                await driver.findElement(By.css('[data-qa="name-on-card"]')).sendKeys('BREDAS BEBRAS');
+                await driver.findElement(By.css('[data-qa="card-number"]')).sendKeys('1234567890123456');
+                await driver.findElement(By.css('[data-qa="cvc"]')).sendKeys('123');
+                await driver.findElement(By.css('[data-qa="expiry-month"]')).sendKeys('10');
+                await driver.findElement(By.css('[data-qa="expiry-year"]')).sendKeys('2030');
+
+                await driver.findElement(By.css('button[data-qa="pay-button"]')).click();
+                await driver.sleep(1000);
+
+                await driver.findElement(By.linkText('Delete Account')).click();
+                const confirmTest = await driver.wait(until.elementLocated(By.xpath("//*[contains(text(),'Account Deleted!')]")), 5000).getText();
+
+                expect(confirmTest).to.equal('ACCOUNT DELETED!');
+                expect(await driver.findElement(By.css('#product-3 .cart_total')).getText()).to.contain('Rs. 1000');
+
+            } catch (error) {
+                console.error("❌ Test failed:", error);
+            } finally {
+                await driver.quit();
+            }
+        });
+
+
 
     });
 })();
