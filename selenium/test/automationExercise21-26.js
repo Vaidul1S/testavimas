b5481f28a468c765fdd59d1801342850c8a6eca2
Vaidul1S <vaidas.ad25@gmail.com>
2025-05-09 12:50:48 +0300

    05.09

--- selenium/test/automationExercise21-26.js
+++ selenium/test/automationExercise21-26.js
@@ -89,18 +89,24 @@ import { expect } from 'chai';
                 await driver.findElement(By.css('button[data-qa="create-account"]')).click();
                 await driver.wait(until.elementLocated(By.xpath("//*[contains(text(),'Account Created!')]")), 5000);
                 await driver.findElement(By.css('[data-qa="continue-button"]')).click();
-
-                
+                expect(await driver.wait(until.elementLocated(By.linkText('Logged in as Bebras666')), 5000).getText()).to.equal('Logged in as Bebras666');
                 
                 await driver.wait(until.elementLocated(By.xpath('//*[@id="recommended-item-carousel"]/div/div[2]/div[2]/div/div/div/a')), 5000).click();
                 await driver.sleep(800);
                 await driver.wait(until.elementLocated(By.xpath('//*[@id="cartModal"]/div/div/div[2]/p[2]/a/u')), 5000).click();
+                
+                await driver.wait(until.urlIs('https://automationexercise.com/view_cart'), 5000);
+                await driver.findElement(By.xpath('//*[@id="do_action"]/div[1]/div/div/a')).click();
 
-                expect(await driver.findElement(By.css('#product-5 .cart_description')).getText()).to.contain('Winter Top');
-                expect(await driver.findElement(By.css('#product-5 .cart_price')).getText()).to.contain('Rs. 600');
-                expect(await driver.findElement(By.css('#product-5 .cart_quantity')).getText()).to.contain('1');
-                expect(await driver.findElement(By.css('#product-5 .cart_total')).getText()).to.contain('Rs. 600');
-
+                expect(await driver.findElement(By.css('#address_delivery .address_firstname ')).getText()).to.contain('Mr. Bredas Babrauskas');
+                expect(await driver.findElement(By.css('#address_delivery .address_address1')).getText()).to.contain('Uztvanka');
+                expect(await driver.findElement(By.css('#address_delivery .address_address1')).getText()).to.contain('Bebriskes');
+                expect(await driver.findElement(By.css('#address_delivery .address_address1')).getText()).to.contain('Rastu11');
+                expect(await driver.findElement(By.css('#address_delivery .address_city ')).getText()).to.contain('Small City Big State 1234567890');
+                expect(await driver.findElement(By.css('#address_delivery .address_country_name')).getText()).to.contain('Canada');
+                expect(await driver.findElement(By.css('#address_delivery .address_phone')).getText()).to.contain('1234567890');
+                
+                
             } catch (error) {
                 console.error("❌ Test failed:", error);
             } finally {
