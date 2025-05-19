666104c472254c54402df6ee170bf179a0bb7435
Vaidul1S <vaidas.ad25@gmail.com>
2025-05-19 12:38:46 +0300

    05.19

--- playtype/tests/automation.spec.ts
+++ playtype/tests/automation.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Automation Exercise Tests', () => {
-    
+
     test.beforeEach(async ({ page }) => {
         await page.goto('https://automationexercise.com/');
         await page.getByRole('button', { name: 'Consent' }).click();
@@ -13,9 +13,33 @@ test.describe('Automation Exercise Tests', () => {
 
         await page.fill('input[data-qa="signup-name"]', 'Bebras666');
         await page.fill('input[data-qa="signup-email"]', 'bebras666@example.com');
+        await page.locator('button[data-qa="signup-button"]').click();
+
+        await expect(page).toHaveTitle(/Enter Account Information/);
+
+        await page.locator('input#id_gender1').click();
+        await page.fill('input[data-qa="password"]', 'password123');
+        await page.fill('select#days', '13');
+        await page.fill('select#months', 'February');
+        await page.fill('select#years', '1999');
+        await page.locator('input#newsletter').click();
+        await page.locator('input#optin').click();
+        await page.fill('input[data-qa="first_name"]', 'Bredas');
+        await page.fill('input[data-qa="last_name"]', 'Babrauskas');
+        await page.fill('input[data-qa="company"]', 'Uzvanka');
+        await page.fill('input[data-qa="address"]', 'Rastu 11');
+        await page.fill('input[data-qa="address2"]', 'Bebriskes');
+        await page.fill('select[data-qa="country"]', 'Canada');
+        await page.fill('input[data-qa="state"]', 'Big State');
+        await page.fill('input[data-qa="city"]', 'Small City');
+        await page.fill('input[data-qa="zipcode"]', '1234567890');
+        await page.fill('input[data-qa="mobile_number"]', '1234567890');
+
+        await page.locator('').click();
+
     });
 
-    
+
     // test('update item', async ({ page }) => {
     //     await page.getByRole('textbox', { name: 'What need\'s to be done?' }).click();
     //     await page.getByRole('textbox', { name: 'What need\'s to be done?' }).fill('123456');
