5db8b5f9d54e19f38fc7eea8a6aa78820dafa300
Vaidul1S <vaidas.ad25@gmail.com>
2025-08-05 11:37:14 +0300

    08.05

--- CRUD_API/seeder/index.js
+++ CRUD_API/seeder/index.js
@@ -16,24 +16,60 @@ const con = mysql.createConnection({
 
 con.connect(_ => console.log('Prisijungta prie duomenų bazės!'));
 
-con.query('DROP TABLE IF EXISTS sessions;'), (err) => {
+con.query('DROP TABLE IF EXISTS users;'), (err) => {
     if (err) throw err;
 }
-con.query('DROP TABLE IF EXISTS clients;'), (err) => {
+con.query('DROP TABLE IF EXISTS products;'), (err) => {
     if (err) throw err;
 }
 
 sql = `
-    CREATE TABLE clients (
+    CREATE TABLE users (
     id int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(32) NOT NULL,
+    username varchar(32) NOT NULL,
     password varchar(255) NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 `;
 
 con.query(sql, (err) => {
     if (err) throw err;
-    console.log('Table Clients created!');
+    console.log('Table Users created!');
+});
+
+sql = `
+    CREATE TABLE products (
+    id int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(32) NOT NULL,
+    price int(11) NOT NULL,
+    description text NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+`;
+
+con.query(sql, (err) => {
+    if (err) throw err;
+    console.log('Table Products created!');
+});
+
+sql = `
+    INSERT INTO users
+    (id, username, password )
+    VALUES ?
+`;
+
+con.query(sql, [users.map(user => [user.id, user.username, user.password])], (err) => {
+    if (err) throw err;
+    console.log('Data inserted into table Users!');
+});
+
+sql = `
+    INSERT INTO products
+    (id, name, price, description )
+    VALUES ?
+`;
+
+con.query(sql, [products.map(product => [product.id, product.name, product.price, product.description])], (err) => {
+    if (err) throw err;
+    console.log('Data inserted into table Products!');
 });
 
 con.end(err => {
