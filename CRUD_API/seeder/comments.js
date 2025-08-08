aa4b72064aec27df7b8152ac441b1eaf9a694eae
Vaidul1S <vaidas.ad25@gmail.com>
2025-08-08 14:14:35 +0300

    08.08

---
+++ CRUD_API/seeder/comments.js
@@ -0,0 +1,11 @@
+export const comments = [
+    { id: 1, user_id: 3, text: 'Nesudegis degiesis', date: '2021-01-01' },
+    { id: 2, user_id: 3, text: 'Baise grazus', date: '2023-08-05' },
+    { id: 3, user_id: 1, text: 'Didle mazas', date: '2022-11-11' },
+    { id: 4, user_id: 5, text: 'Krami', date: '2021-03-14' },
+    { id: 5, user_id: 5, text: 'Kon jus, furista?', date: '1986-04-26' },
+    { id: 6, user_id: 1, text: 'Pri meskos', date: '2024-09-03' },
+    { id: 7, user_id: 2, text: 'Baravyku, raudonikiu', date: '2025-08-04' },
+    
+    
+];
--- CRUD_API/seeder/index.js
+++ CRUD_API/seeder/index.js
@@ -1,5 +1,6 @@
 import { users } from './users.js';
 import { products } from './products.js';
+import { comments } from './comments.js';
 import mysql from 'mysql';
 import md5 from 'md5';
 
@@ -17,6 +18,9 @@ const con = mysql.createConnection({
 
 con.connect(_ => console.log('Prisijungta prie duomenų bazės!'));
 
+con.query('DROP TABLE IF EXISTS comments;'), (err) => {
+    if (err) throw err;
+}
 con.query('DROP TABLE IF EXISTS users;'), (err) => {
     if (err) throw err;
 }
@@ -52,6 +56,30 @@ con.query(sql, (err) => {
 });
 
 sql = `
+    CREATE TABLE comments (
+    id int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id int(11) UNSIGNED NOT NULL,
+    date date NOT NULL,
+    text text NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+`;
+
+con.query(sql, (err) => {
+    if (err) throw err;
+    console.log('Table Comments created!');
+});
+
+sql = `
+    ALTER TABLE comments
+    ADD CONSTRAINT comments_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id);
+`;
+
+con.query(sql, (err) => {
+    if (err) throw err;
+    console.log('Table keys created!');
+});
+
+sql = `
     INSERT INTO users
     (id, username, password )
     VALUES ?
@@ -60,6 +88,17 @@ sql = `
 con.query(sql, [users.map(user => [user.id, user.username, user.password])], (err) => {
     if (err) throw err;
     console.log('Data inserted into table Users!');
+});
+
+sql = `
+    INSERT INTO comments
+    (id, user_id, date, text )
+    VALUES ?
+`;
+
+con.query(sql, [comments.map(comment => [comment.id, comment.user_id, comment.date, comment.text])], (err) => {
+    if (err) throw err;
+    console.log('Data inserted into table Comments!');
 });
 
 sql = `
